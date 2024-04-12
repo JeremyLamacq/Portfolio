92667ac440e93781a56f13a0d7b11eec5fa6215e
JeremyLamacq <jeremy.lamacq@oclock.school>
2024-04-12 15:21:05 +0200

    entry home transition ok

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,5 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
-// import Loading from '../loading/Loading';
 import HomePage from '../homePage/HomePage';
 import Header from '../header/Header';
 import Contact from '../contact/Contact';
@@ -8,15 +7,17 @@ import Skills from '../skills/Skills';
 import Experience from '../experience/Experience';
 import Footer from '../footer/Footer';
 import ErrorPage from '../error/ErrorPage';
+import LoadingComponent from '../loadingComponent/LoadingComponent';
 
-function App() {
+
+const App = () => {
 
   return (
     <>
+      <LoadingComponent />
       <Header />
         <Routes>
-          {/* <Route path='/loading' element= {<Loading />} /> */}
-          <Route path='/' element= {<HomePage /> }/>
+          <Route path='/' element={<HomePage />} />
           <Route path="*" element={<ErrorPage />} />
           <Route path='/experience' element={<Experience />} />
           <Route path='/skills' element={<Skills />} />
---
+++ src/components/loadingComponent/LoadingComponent.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import './LoadingComponent.scss';
+
+const LoadingComponent = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <main id='mainLoader' style={{ opacity: isLoading ? 1 : 0, transition: 'opacity 0.5s ease', display: !isLoading ? 'none' : '' }}>
+        <h1>Bienvenu sur mon Portfolio</h1>
+    </main>
+  );
+};
+
+export default LoadingComponent;
